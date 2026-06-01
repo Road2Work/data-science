7dd8523988dad5f6925b4a1fa9d965ef4dd4791e
nurulainilf <nurulainilf@gmail.com>
2026-06-01 15:41:26 +0700

    streamlit cloud connection

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -11,7 +11,7 @@ import os
 # =====================================================================
 st.set_page_config(
     page_title="Road2Work Analytics",
-    page_icon="🎯",
+    page_icon="icon.png",
     layout="wide",
     initial_sidebar_state="expanded"
 )
